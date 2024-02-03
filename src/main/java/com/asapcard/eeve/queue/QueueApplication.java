4f1645663a428c5ef18386330414dbc47ef350c6
Daniela Xavier <danielamxsoares@gmail.com>
2024-02-03 18:54:23 -0300

    feat: montar estrutura do projeto

---
+++ src/main/java/com/asapcard/eeve/queue/QueueApplication.java
@@ -0,0 +1,13 @@
+package com.asapcard.eeve.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asapcard/eeve/queue/QueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asapcard.eeve.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
